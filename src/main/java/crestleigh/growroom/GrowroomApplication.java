ce2d4d2b68fdd0c31d46945d052a0ff1f5dd4c9b
crestleigh <pete@peteolson.net>
2020-02-16 07:14:34 -0500

    init

---
+++ src/main/java/crestleigh/growroom/GrowroomApplication.java
@@ -0,0 +1,13 @@
+package crestleigh.growroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowroomApplication.class, args);
+	}
+
+}
---
+++ src/main/java/crestleigh/growroom/HomeController.java
@@ -0,0 +1,15 @@
+package crestleigh.growroom;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping("/")
+	String hello() {
+		return "index";
+	}
+	
+}
