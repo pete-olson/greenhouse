184bc9475ffcfc58d709113959cf9951df0f428b
crestleigh <pete@peteolson.net>
2020-02-16 10:55:57 -0500

    init

--- src/main/java/crestleigh/growroom/GrowroomApplication.java
+++ src/main/java/crestleigh/growroom/GrowroomApplication.java
@@ -2,9 +2,14 @@ package crestleigh.growroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GrowroomApplication {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GrowroomApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrowroomApplication.class, args);
